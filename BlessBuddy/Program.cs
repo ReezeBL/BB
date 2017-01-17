230d644a82927e4a26f97417b3751a308609c444
Evgeniy Shilkin <shilk.e82@gmail.com>
2017-01-17 15:36:08 +0300

    initial commit

--- BlessBuddy/Program.cs
+++ BlessBuddy/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SlimDX.Direct3D9;
 
 namespace BlessBuddy
 {
@@ -10,6 +6,12 @@ namespace BlessBuddy
     {
         static void Main(string[] args)
         {
+            var device = new Device(
+                new Direct3D(),
+                0,
+                DeviceType.Hardware,
+                System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle,
+                CreateFlags.HardwareVertexProcessing, new PresentParameters());
         }
     }
 }
